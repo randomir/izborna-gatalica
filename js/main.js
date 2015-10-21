8d2d9408a1e012d6044b3f3360945ee95c7ba413
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2015-10-21 02:37:12 +0200

    added toggle button to keep parties sorted by match percent

--- js/main.js
+++ js/main.js
@@ -71,16 +71,9 @@ function renderQuestions($target, answers) {
             return answerDesc[value];
         }
     });
-    //$(".answer-slider").on('change', function(e) {
-    //    var slider = $("#"+$(this).data('slider-id'));
-    //    $(".slider-selection", slider).css({background: answerColors[e.value.newValue]});
-    //});
     $(".answer-slider").on('change', function(e) {
-        var userScores = $(".answer-slider").map(function() {
-            return $(this).data('slider').getValue();
-        });
-        updatePartyMatches(userScores);
-    });
+        updatePartyMatches(getUserScores());
+    })
 }
 
 function renderParties($target) {
@@ -143,7 +136,10 @@ function match(a, b) {
 function calcPartyMatches(userScores) {
     var matches = [];
     for (var i = 0; i < partyScores.length; i++) {
-        matches.push(match(partyScores[i], userScores));
+        matches.push({
+            id: i,
+            score: match(partyScores[i], userScores)
+        });
     }
     return matches;
 }
@@ -152,9 +148,35 @@ function updatePartyMatches(userScores) {
     var matches = calcPartyMatches(userScores);
     $(".party-score").each(function() {
         var elem = $(this), id = elem.data('party-id'),
-            val = Math.round(matches[id]), percent = val+'%';
+            val = Math.round(matches[id].score), percent = val+'%';
         elem.data('aria-valuenow', val).css({width: percent}).text(percent);
     });
+    if ($("#keep-parties-sorted").is(".active")) {
+        orderPartiesByMatching(matches);
+    }
+}
+
+function getUserScores() {
+    return $(".answer-slider").map(function() {
+        return $(this).data('slider').getValue();
+    });
+}
+
+function orderPartiesByMatching(matches) {
+    var ordered = matches.slice(), $box = $("#results");
+    ordered.sort(function(a, b) {
+        return +(a.score > b.score) || +(a.score === b.score) - 1;
+    });
+    for (var i = 0; i < ordered.length; i++) {
+        var p = ordered[i];
+        $(".party-match[data-party-id="+p.id+"]").detach().prependTo($box);
+    }
+}
+
+function orderPartiesById() {
+    for (var i = 0; i < partyNames.length; i++) {
+        $(".party-match[data-party-id="+i+"]").detach().appendTo("#results");
+    }
 }
 
 $(function() {
@@ -166,6 +188,15 @@ $(function() {
         var scores = partyScores[id];
         renderQuestions($("#questions"), scores);
         updatePartyMatches(scores);
+        return false;
+    });
+    $("#keep-parties-sorted").on('click', function() {
+        var elem = $(this);
+        elem.toggleClass('active');
+        if (!elem.hasClass('active')) {
+            orderPartiesById();
+        } else {
+            updatePartyMatches(getUserScores());
+        }
     });
 });
-
