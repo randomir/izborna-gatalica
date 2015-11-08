85efa85331d5f9a44c80b623ddcf5218cd303963
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2015-11-08 04:00:37 +0100

    decision tree of limited depth trained on a large uniform sample

--- train/train.py
+++ train/train.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import numpy as np
 import json
+from itertools import izip
+from functools import partial
 from sklearn import tree
 from sklearn.externals.six import StringIO
 
@@ -37,17 +39,33 @@ def generate(n_samples, scores_norm):
         Y.append(classify(x, scores_norm))
     return X, Y
 
+def generate_distrib(n_samples_per_class, scores_norm):
+    """Generate a set of points with `n_samples_per_class` distribution of
+    point's classes."""
+    n_samples = sum(n_samples_per_class)
+    X, Y = [], []
+    while len(Y) < n_samples:
+        Xi, Yi = generate(n_samples, scores_norm)
+        for x, y in izip(Xi, Yi):
+            if n_samples_per_class[y] > 0:
+                X.append(list(x))
+                Y.append(y)
+                n_samples_per_class[y] -= 1
+    return X, Y
+
 def train(X, Y, **kwargs):
     clf = tree.DecisionTreeClassifier(**kwargs)
     clf = clf.fit(X, Y)
     return clf
 
-def export_dot(clf, filename):
+def export_dot(clf, filename, **kwargs):
     with open(filename, 'w') as f:
-        tree.export_graphviz(clf, out_file=f)
+        tree.export_graphviz(clf, out_file=f, **kwargs)
 
 def export_json(clf, filename):
     t = clf.tree_
+    N = t.n_node_samples[0]
+    round2 = partial(round, ndigits=2)
     with open(filename, 'w') as f:
         json.dump({
             "n_nodes": t.node_count,
@@ -57,17 +75,20 @@ def export_json(clf, filename):
             "children_left": t.children_left.tolist(),
             "children_right": t.children_right.tolist(),
             "gini": t.impurity.tolist(),
-            "n_samples": t.n_node_samples.tolist(),
-            "value": t.n_node_samples.tolist(),
+            "n_samples": [round2(100.0*n/N) for n in
+                          t.n_node_samples.tolist()],
+            "distribution": [map(round2, 100.0*x/n) for x, n in
+                             izip(t.value[:,0], t.n_node_samples)],
         }, fp=f, indent=2)
 
 
 if __name__ == '__main__':
-    X, Y = generate(1000, scores_norm)
-    clf = train(X, Y, max_depth=10)
-    export_dot(clf, 'tree.dot')
+    # train test (overfit)
+    #clf12 = train(scores, np.arange(0, 12))
+    #export_dot(clf12, '12.dot')
+    #export_json(clf12, '12.json')
+
+    X, Y = generate_distrib(len(scores_norm) * [1000000], scores_norm)
+    clf = train(X, Y, max_depth=7)
+    export_dot(clf, 'tree.dot', proportion=True)
     export_json(clf, 'tree.json')
-    
-    clf12 = train(scores, np.arange(0, 12))
-    export_dot(clf12, '12.dot')
-    export_json(clf12, '12.json')
